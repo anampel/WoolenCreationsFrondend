0b418559861bc3662d8b09a2250977d4c94706ff
Ana Mpel <anampel.am@gmail.com>
2022-05-16 22:37:28 +0300

    Starting Version 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AppService} from '../app.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public currentUser;
+  constructor(private productService: AppService) {
+    this.currentUser = localStorage.getItem('currentUser') ? JSON.parse(localStorage.getItem('currentUser')) : '';
   }
-  slideConfig = {"slidesToShow": 1, "slidesToScroll": 1} ;
+  public fetchedProducts: any[];
+  public fetchedOffers: any[];
+
+  ngOnInit() {
+    this.productService.findAllProducts().subscribe(
+      (response: any[]) => this.fetchedProducts = response
+    );
+    this.productService.findAllActiveOffers().subscribe(
+      (response: any[]) => this.fetchedOffers = response
+    );
+  }
 }
